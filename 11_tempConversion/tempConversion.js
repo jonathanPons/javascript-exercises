a88ee143e75ca2ff35fb49e099dfb2158401a57d
Joponspolo <159774189+jonathanPons@users.noreply.github.com>
2025-12-10 08:07:46 +0100

    travail exercice 7/11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempCel) {
+  let tempCel = 0;
+  return tempCel / 32;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempFar) {
+  return tempFar * 32;
 };
+console.log(convertToCelsius(32));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
